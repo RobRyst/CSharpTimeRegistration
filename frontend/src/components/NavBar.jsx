83785eb1e709497ff0d957b3d28498e65da541b9
Ryztard <Ryztad@live.com>
2025-08-08 21:17:20 +0200

    Making it so User roles cannot see Projects or Stats

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,28 @@
 import { NavLink } from "react-router";
 import LogoutBtn from "./Logoutbtn";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) return;
+
+    try {
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      const roles =
+        payload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+
+      setIsAdmin(
+        Array.isArray(roles) ? roles.includes("Admin") : roles === "Admin"
+      );
+    } catch (err) {
+      console.error("Failed to decode token", err);
+      setIsAdmin(false);
+    }
+  }, []);
+
   return (
     <>
       <div className="flex flex-row justify-between items-center bg-zinc-800">
@@ -15,14 +36,18 @@ const NavBar = () => {
             <NavLink to="/" className="text-white">
               Home
             </NavLink>
-            <NavLink to="/projects" className="text-white">
-              Projects
-            </NavLink>
+            {isAdmin && (
+              <>
+                <NavLink to="/projects" className="text-white">
+                  Projects
+                </NavLink>
+                <NavLink to="/statistics" className="text-white">
+                  Statistics
+                </NavLink>
+              </>
+            )}
             <NavLink to="/overview" className="text-white">
               Overview
-            </NavLink>
-            <NavLink to="/statistics" className="text-white">
-              Statistics
             </NavLink>
             <NavLink to="/profile" className="text-white">
               Profile
