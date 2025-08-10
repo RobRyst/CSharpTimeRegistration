299b877a427ed43c19986545fa0e6a4fe3868059
Ryztard <Ryztad@live.com>
2025-08-10 16:58:47 +0200

    test

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83785eb1e709497ff0d957b3d28498e65da541b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc537089ae56b6a1890df80faeb27ad6b76c5725")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
